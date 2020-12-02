b2b892ea60ef2a133144c37fee7756927fb3e7ae
Milan Stute <mstute@seemoo.tu-darmstadt.de>
2020-12-02 10:24:09 +0100

    Indicate Python 3.8 and 3.9 support

--- setup.py
+++ setup.py
@@ -29,6 +29,8 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     keywords="cli",
     packages=find_packages(exclude=["docs"]),
